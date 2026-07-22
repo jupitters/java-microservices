cf38619c203e65f34ad9f277619b403728355436
jupitters <sampaiobale@gmail.com>
2026-07-22 16:29:11 -0300

    product controller

--- src/main/java/com/jupitters/product_service/controller/ProductController.java
+++ src/main/java/com/jupitters/product_service/controller/ProductController.java
@@ -1,9 +1,16 @@
 package com.jupitters.product_service.controller;
 
+import com.jupitters.product_service.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+
+    public ResponseEntity<Product> createProduct(@RequestBody ProductRequest product) {
+
+    }
 }
