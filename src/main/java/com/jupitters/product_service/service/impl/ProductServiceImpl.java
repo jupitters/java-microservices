eca08fbc87ceab0ed93a3dde39e6857a32ba4104
jupitters <sampaiobale@gmail.com>
2026-07-24 11:55:57 -0300

    service create product

--- src/main/java/com/jupitters/product_service/service/impl/ProductServiceImpl.java
+++ src/main/java/com/jupitters/product_service/service/impl/ProductServiceImpl.java
@@ -1,8 +1,22 @@
 package com.jupitters.product_service.service.impl;
 
+import com.jupitters.product_service.dto.ProductRequest;
+import com.jupitters.product_service.model.Product;
+import com.jupitters.product_service.repository.ProductRepository;
 import com.jupitters.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    public Product createProduct(ProductRequest request) {
+        Product product = Product.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .price(request.getPrice())
+                .build();
+    }
 }
