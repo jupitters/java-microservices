c8bd79dff5395827b26a45e18136101e443febd4
jupitters <sampaiobale@gmail.com>
2026-07-31 17:54:11 -0300

    service get product by id

--- src/main/java/com/jupitters/product_service/service/impl/ProductServiceImpl.java
+++ src/main/java/com/jupitters/product_service/service/impl/ProductServiceImpl.java
@@ -29,4 +29,9 @@ public class ProductServiceImpl implements ProductService {
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
+
+    @Override
+    public Product getProductById() {
+        return null;
+    }
 }
