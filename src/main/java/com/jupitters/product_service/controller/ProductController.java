9f530adfb4c28a77e8fd54daaf06a18109b6da00
jupitters <sampaiobale@gmail.com>
2026-07-30 21:19:24 -0300

    controller get all products

--- src/main/java/com/jupitters/product_service/controller/ProductController.java
+++ src/main/java/com/jupitters/product_service/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
@@ -21,7 +23,7 @@ public class ProductController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<Product> getAllProducts() {
+    public ResponseEntity<List<Product>> getAllProducts() {
         return productService.getAllProducts();
     }
 }
