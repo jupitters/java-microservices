e4f28156f8f44b56a2c13df717a87cfca067dab5
jupitters <sampaiobale@gmail.com>
2026-07-30 21:14:48 -0300

    service get all products

--- src/main/java/com/jupitters/product_service/service/ProductService.java
+++ src/main/java/com/jupitters/product_service/service/ProductService.java
@@ -3,6 +3,9 @@ package com.jupitters.product_service.service;
 import com.jupitters.product_service.dto.ProductRequest;
 import com.jupitters.product_service.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product createProduct(ProductRequest request);
+    List<Product> getAllProducts();
 }
