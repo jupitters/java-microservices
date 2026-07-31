80dd26e30a6dfda386fdc7c72c3698a292892aa3
jupitters <sampaiobale@gmail.com>
2026-07-30 21:17:50 -0300

    controller create

--- src/main/java/com/jupitters/product_service/controller/ProductController.java
+++ src/main/java/com/jupitters/product_service/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.jupitters.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final ProductService productService;
 
+    @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody ProductRequest product) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productService.createProduct(product));
