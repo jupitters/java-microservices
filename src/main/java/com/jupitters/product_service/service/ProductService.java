d96e23d62f432f5a994cb83940e58d82627565e1
jupitters <sampaiobale@gmail.com>
2026-08-03 19:49:34 -0300

    adding product response to service

--- src/main/java/com/jupitters/product_service/service/ProductService.java
+++ src/main/java/com/jupitters/product_service/service/ProductService.java
@@ -1,12 +1,13 @@
 package com.jupitters.product_service.service;
 
 import com.jupitters.product_service.dto.ProductRequest;
+import com.jupitters.product_service.dto.ProductResponse;
 import com.jupitters.product_service.model.Product;
 
 import java.util.List;
 
 public interface ProductService {
     Product createProduct(ProductRequest request);
-    List<Product> getAllProducts();
+    List<ProductResponse> getAllProducts();
     Product getProductById();
 }
