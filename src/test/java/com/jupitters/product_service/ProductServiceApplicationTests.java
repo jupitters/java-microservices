8f5eaf4c39c2b4d96e7a3b8cc6279c1e52f48bae
jupitters <sampaiobale@gmail.com>
2026-08-03 21:50:26 -0300

    test setup

--- src/test/java/com/jupitters/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/jupitters/product_service/ProductServiceApplicationTests.java
@@ -1,14 +1,32 @@
 package com.jupitters.product_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.mongodb.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ProductServiceApplicationTests {
+	@ServiceConnection
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
 
 	@Test
 	void shouldCreateProduct() {
