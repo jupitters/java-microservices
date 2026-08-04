24281a7e3d6e5dc3be134bd6f6a5448b93ebedf5
jupitters <sampaiobale@gmail.com>
2026-08-03 21:56:59 -0300

    tests

--- src/test/java/com/jupitters/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/jupitters/product_service/ProductServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.jupitters.product_service;
 
 import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,6 +33,25 @@ class ProductServiceApplicationTests {
 
 	@Test
 	void shouldCreateProduct() {
+		String requestBody = """
+				{
+					"name": "Iphone 15",
+					"description": "A smartphone from Apple",
+					"price": 1200
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Iphone 15"))
+				.body("description", Matchers.equalTo("A smartphone from Apple"))
+				.body("price", Matchers.equalTo(1200));
 	}
 
 }
