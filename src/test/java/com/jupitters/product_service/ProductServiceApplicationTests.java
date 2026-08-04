6c4ca2ecdb4b2f1d5fe7b748bde92de658654d5d
jupitters <sampaiobale@gmail.com>
2026-08-03 21:44:06 -0300

    application and tests

--- src/test/java/com/jupitters/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/jupitters/product_service/ProductServiceApplicationTests.java
@@ -3,13 +3,15 @@ package com.jupitters.product_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ProductServiceApplicationTests {
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
 
 	@Test
-	void contextLoads() {
+	void shouldcCeateProduct() {
 	}
 
 }
