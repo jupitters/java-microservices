5289d8778da19c14b9b1de79493a037fbc8300d7
jupitters <sampaiobale@gmail.com>
2026-08-04 21:13:50 -0300

    order entity

--- order-services/src/main/java/com/jupitters/order_services/model/Order.java
+++ order-services/src/main/java/com/jupitters/order_services/model/Order.java
@@ -1,10 +1,19 @@
 package com.jupitters.order_services.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
