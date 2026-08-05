41c574d0a49a98c8c0f723d1d39baa1ae67efbf1
jupitters <sampaiobale@gmail.com>
2026-08-05 19:26:50 -0300

    service place order

--- order-services/src/main/java/com/jupitters/order_services/service/impl/OrderServiceImpl.java
+++ order-services/src/main/java/com/jupitters/order_services/service/impl/OrderServiceImpl.java
@@ -1,8 +1,16 @@
 package com.jupitters.order_services.service.impl;
 
+import com.jupitters.order_services.repository.OrderRepository;
 import com.jupitters.order_services.service.OrderSerivce;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderSerivce {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest request){
+
+    }
 }
