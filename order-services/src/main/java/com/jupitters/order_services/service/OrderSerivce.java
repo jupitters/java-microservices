8e51ea4fa612048cb88cb617b37f42ab17ac0461
jupitters <sampaiobale@gmail.com>
2026-08-05 19:25:25 -0300

    order service

---
+++ order-services/src/main/java/com/jupitters/order_services/service/OrderSerivce.java
@@ -0,0 +1,4 @@
+package com.jupitters.order_services.service;
+
+public class OrderSerivce {
+}
---
+++ order-services/src/main/java/com/jupitters/order_services/service/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jupitters.order_services.service.impl;
+
+import com.jupitters.order_services.service.OrderSerivce;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderSerivce {
+}
