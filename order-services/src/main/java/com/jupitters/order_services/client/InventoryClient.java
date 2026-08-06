7a608c20a11bcd2ed77333189231bed2f327ccae
jupitters <sampaiobale@gmail.com>
2026-08-06 15:47:56 -0300

    inventory client

--- order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
+++ order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
     @RequestMapping(value = "/api/inventory", method = RequestMethod.GET)
-    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
