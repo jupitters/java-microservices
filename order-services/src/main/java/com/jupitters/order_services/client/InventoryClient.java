aac5d81c65c2891149383a78f0a2e99e8e3e98e2
jupitters <sampaiobale@gmail.com>
2026-08-06 15:47:50 -0300

    inventory client

--- order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
+++ order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
@@ -1,4 +1,12 @@
 package com.jupitters.order_services.client;
 
-public class InventoryClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
+public interface InventoryClient {
+    @RequestMapping(value = "/api/inventory", method = RequestMethod.GET)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
