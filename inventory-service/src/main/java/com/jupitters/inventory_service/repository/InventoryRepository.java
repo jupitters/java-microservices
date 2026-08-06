fe1fe562404b534e2bb559ba8c29b9a3f7281e4e
jupitters <sampaiobale@gmail.com>
2026-08-05 22:16:24 -0300

    inventory repository

---
+++ inventory-service/src/main/java/com/jupitters/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,7 @@
+package com.jupitters.inventory_service.repository;
+
+import com.jupitters.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+}
