650da471d7572d137d84109e52998c8b6ccb86ed
jupitters <sampaiobale@gmail.com>
2026-08-06 17:05:57 -0300

    test

--- order-services/src/test/java/com/jupitters/order_services/OrderServicesApplicationTests.java
+++ order-services/src/test/java/com/jupitters/order_services/OrderServicesApplicationTests.java
@@ -1,12 +1,34 @@
 package com.jupitters.order_services;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mysql.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc()
 class OrderServicesApplicationTests {
+
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static{
+		mySQLContainer.start();
+	}
 
 	@Test
 	void contextLoads() {
