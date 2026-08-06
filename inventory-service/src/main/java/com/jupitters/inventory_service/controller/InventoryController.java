f0877d8e2c1fc72c91f834fb55b5b81fe4a9198e
jupitters <sampaiobale@gmail.com>
2026-08-05 22:26:12 -0300

    inventory controller

--- inventory-service/src/main/java/com/jupitters/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/jupitters/inventory_service/controller/InventoryController.java
@@ -2,9 +2,9 @@ package com.jupitters.inventory_service.controller;
 
 import com.jupitters.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryController {
     private final InventoryService inventoryService;
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
         return inventoryService.isInStock(skuCode, quantity);
     }
