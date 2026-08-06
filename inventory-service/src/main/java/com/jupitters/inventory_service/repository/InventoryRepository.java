eb221661b1e74b470ecf103aa2bdf7bab39358bd
jupitters <sampaiobale@gmail.com>
2026-08-05 22:19:17 -0300

    inventory service

--- inventory-service/src/main/java/com/jupitters/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/jupitters/inventory_service/repository/InventoryRepository.java
@@ -4,4 +4,5 @@ import com.jupitters.inventory_service.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityGreaterThanEquals(String skuCode, Integer quantity);
 }
