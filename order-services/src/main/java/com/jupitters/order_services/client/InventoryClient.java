0cb6d61ddb0fc7af0e0c9be4786d0f07dcd191e4
jupitters <sampaiobale@gmail.com>
2026-08-06 21:55:46 -0300

    updating values

--- order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
+++ order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
     @RequestMapping(value = "/api/inventory", method = RequestMethod.GET)
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
