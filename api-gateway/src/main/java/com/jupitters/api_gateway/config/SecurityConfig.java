11ad8d8a762d9b8b1fa715d234c7aa629689c873
jupitters <sampaiobale@gmail.com>
2026-08-07 13:38:35 -0300

    security filter chain

--- api-gateway/src/main/java/com/jupitters/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/jupitters/api_gateway/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest()
+        return http.authorizeHttpRequests(auth -> auth.anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
