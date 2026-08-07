96530b4faac3f51f86eddd1c8143011ddf0686e7
jupitters <sampaiobale@gmail.com>
2026-08-06 23:23:42 -0300

    tests

--- order-services/src/test/java/com/jupitters/order_services/OrderServicesApplicationTests.java
+++ order-services/src/test/java/com/jupitters/order_services/OrderServicesApplicationTests.java
@@ -16,7 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc()
+@AutoConfigureWireMock(port = 0)
 class OrderServicesApplicationTests {
 
 	@ServiceConnection
