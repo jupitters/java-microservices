e64821a85be722ad538d567e604572237c2b7934
jupitters <sampaiobale@gmail.com>
2026-08-07 13:35:48 -0300

    fix dependency

--- api-gateway/src/main/java/com/jupitters/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/jupitters/api_gateway/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.jupitters.api_gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(Http) {
+
+    }
 }
