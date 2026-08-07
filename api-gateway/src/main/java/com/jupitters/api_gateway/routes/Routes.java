0426d1377bcd8571deeffa911320c9a4753f33cf
jupitters <sampaiobale@gmail.com>
2026-08-07 12:19:34 -0300

    route for product service

--- api-gateway/src/main/java/com/jupitters/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/jupitters/api_gateway/routes/Routes.java
@@ -1,7 +1,22 @@
 package com.jupitters.api_gateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
 
 @Configuration
 public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product_service")
+                .route(
+                        RequestPredicates.path("/api/product"),
+                        HandlerFunctions.http()
+                )
+                .before(BeforeFilterFunctions.uri("http://localhost:8080"))
+                .build();
+    }
 }
