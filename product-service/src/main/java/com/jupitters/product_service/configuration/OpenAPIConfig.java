9bbf55ee7e5607135b181bc8a15338771a598665
jupitters <sampaiobale@gmail.com>
2026-08-10 17:23:07 -0300

    swagger info

---
+++ product-service/src/main/java/com/jupitters/product_service/configuration/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.jupitters.product_service.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI productServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Product Service API")
+                        .description("This is the REST API for Product Service")
+                        .version("v0.0.1")
+                        .license(new License().name("This is the license")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Refer to the service wiki.")
+                        .url("http://dummy-url.com"));
+    }
+}
