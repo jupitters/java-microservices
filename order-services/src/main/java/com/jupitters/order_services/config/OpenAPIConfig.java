cfd3597947d62019a512d892f983a40695da2cf4
jupitters <sampaiobale@gmail.com>
2026-08-11 14:30:49 -0300

    swagger info

--- order-services/src/main/java/com/jupitters/order_services/config/OpenAPIConfig.java
+++ order-services/src/main/java/com/jupitters/order_services/config/OpenAPIConfig.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
+    @Bean
     public OpenAPI orderServiceAPI() {
         return new OpenAPI()
                 .info(new Info().title("Order Service API")
