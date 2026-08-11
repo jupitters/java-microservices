3c59ba8005b6c934f559a1115b7d2db77ec6c309
jupitters <sampaiobale@gmail.com>
2026-08-11 20:35:30 -0300

    migration to rest client

--- order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
+++ order-services/src/main/java/com/jupitters/order_services/client/InventoryClient.java
@@ -1,12 +1,9 @@
 package com.jupitters.order_services.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
-    @RequestMapping(value = "/api/inventory", method = RequestMethod.GET)
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
