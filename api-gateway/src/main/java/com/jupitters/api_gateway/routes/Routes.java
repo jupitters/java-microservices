5566f053a2a00ba061b37ee5c23fcf6bf47d65ea
jupitters <sampaiobale@gmail.com>
2026-08-12 15:02:48 -0300

    route fallback

--- api-gateway/src/main/java/com/jupitters/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/jupitters/api_gateway/routes/Routes.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.*;
 
-import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
 
 @Configuration
 public class Routes {
@@ -77,6 +77,13 @@ public class Routes {
                 )
                 .before(BeforeFilterFunctions.uri("http://localhost:8082"))
                 .before(BeforeFilterFunctions.setPath("/v3/api-docs"))
+                .build();
+    }
+
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable"))
                 .build();
     }
 }
