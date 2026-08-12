17f670a08b570e591b04966a649ae032e4981ec8
jupitters <sampaiobale@gmail.com>
2026-08-11 21:22:11 -0300

    ?

--- inventory-service/src/main/java/com/jupitters/inventory_service/config/OpenAPIConfig.java
+++ inventory-service/src/main/java/com/jupitters/inventory_service/config/OpenAPIConfig.java
@@ -1,5 +1,7 @@
 package com.jupitters.inventory_service.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
