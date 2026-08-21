714fdf3701103f0a32d88bd83046f6f57bd8e72a
jupitters <sampaiobale@gmail.com>
2026-08-21 11:29:54 -0300

    add loki

--- api-gateway/src/main/java/com/jupitters/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/jupitters/api_gateway/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(freeResourceUrls).permitAll()
                         .anyRequest().permitAll())
